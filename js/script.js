d23c42cc01058f8f178f2ff9c1634daf587ad06a
Luis Antonio <antonhy.romero98@gmail.com>
2022-03-19 01:45:57 -0600

    La funcion para los pads ahora puede administrar el espacio del boton  en base a la data suministrada.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 createPad("number-pad", [9,8,7,6,5,4,3,2,1,0], "30");
-createPad("operator-pad",["DEL","CLEAR","*","/","+","-","="], "45");
+createPad("operator-pad",["DEL","CLEAR","*","/","+","-",["=",2]], "45");
 
 const screen = document.getElementById("calculator-screen");
 
@@ -8,15 +8,20 @@ function createPad(padId, dataForButtons, porcentageLength){
 
     for(const data of dataForButtons){
         const button = document.createElement("button");
-        button.style.width = porcentageLength + "%";
-        button.textContent = data;
+        if(Array.isArray(data)){
+            button.style.width = porcentageLength * data[1] + "%";
+            button.textContent = data[0];
+        }else{
+            button.style.width = porcentageLength + "%";
+            button.textContent = data;
+        }
         button.addEventListener("click", () =>{
-            if(data === "DEL"){
+            if(button.textContent === "DEL"){
                 screen.textContent = screen.textContent.slice(0,-1);
-            }else if(data === "CLEAR"){
+            }else if(button.textContent === "CLEAR"){
                 clearScreen();
             }else{
-                screen.textContent += data;
+                screen.textContent += button.textContent;
             }
         });
         pad.appendChild(button);
