18c7fc650c0db9019ac24ee1b08d796d5f39af48
Luis Antonio <antonhy.romero98@gmail.com>
2022-03-20 01:59:43 -0600

    Las operaciones de la calculadora ya funcionan correctamente, incluyendo operaciones seguidas. Tambien ya redondea numeros decimales a dos digitos. Falta testear mas funciones.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 createPad("number-pad", [9,8,7,6,5,4,3,2,1,0], "30");
 createPad("operator-pad",["DEL","CLEAR","*","/","+","-",["=",2]], "45");
 
-const screen = document.getElementById("calculator-screen");
+const upperScreen = document.getElementById("upper-screen")
+const lowerScreen = document.getElementById("lower-screen");
+let firstNumber = null, secondNumber = null, operator = "", clear = false;
 
 function createPad(padId, dataForButtons, porcentageLength){
     const pad = document.getElementById(padId);
@@ -17,17 +19,78 @@ function createPad(padId, dataForButtons, porcentageLength){
         }
         button.addEventListener("click", () =>{
             if(button.textContent === "DEL"){
-                screen.textContent = screen.textContent.slice(0,-1);
+                lowerScreen.textContent = lowerScreen.textContent.slice(0,-1);
+                if(lowerScreen.textContent === ""){
+                    lowerScreen.textContent = "0";
+                }
+                clear = false;
             }else if(button.textContent === "CLEAR"){
                 clearScreen();
+            }else if(button.textContent === "="){
+                pushedEquals();
+            }else if(!isNaN(button.textContent)){
+                pushedNumber(button.textContent);
             }else{
-                screen.textContent += button.textContent;
+                pushedOperator(button.textContent);
             }
         });
         pad.appendChild(button);
     }
 }
 
+function pushedOperator(buttonContent){
+    if(!operator){
+        upperScreen.textContent = `${lowerScreen.textContent} ${buttonContent} `;
+        operator = buttonContent;
+        firstNumber = Number(lowerScreen.textContent);
+        lowerScreen.textContent = "0";
+    }else{
+        secondNumber = Number(lowerScreen.textContent);
+        firstNumber = operate(firstNumber, operator, secondNumber);
+        operator = buttonContent;
+        upperScreen.textContent = `${firstNumber} ${operator} `;
+        lowerScreen.textContent = firstNumber;
+        clear = true;
+    }
+}
+
+function pushedNumber(buttonContent){
+    if(lowerScreen.textContent === "0" || clear){
+        lowerScreen.textContent = buttonContent;
+        clear = false;
+    }else{
+        lowerScreen.textContent += buttonContent;
+    }
+}
+
+function pushedEquals(){
+    if(operator && firstNumber !== null){
+        secondNumber = Number(lowerScreen.textContent);
+        lowerScreen.textContent = operate(firstNumber, operator, secondNumber);
+        upperScreen.textContent += `${secondNumber} = `;
+        firstNumber = null, secondNumber = null, operator = "", clear = false; 
+    }
+}
+
 function clearScreen(){
-    screen.textContent = "";
+    upperScreen.textContent = "";
+    lowerScreen.textContent = "0";
+    firstNumber = null, secondNumber = null, operator = "", clear = false;
+}
+
+function operate(num1,operator,num2){
+    let result = 0;
+
+    switch(operator){
+        case "+": result = num1 + num2;
+            break; 
+        case "-": result = num1 - num2;
+            break;
+        case "*": result = num1 * num2;
+            break;
+        case "/": result = Math.round((num1 / num2 + Number.EPSILON) * 100) / 100;
+            break;
+    }
+
+    return result;
 }
